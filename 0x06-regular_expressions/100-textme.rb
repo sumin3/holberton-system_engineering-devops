c8ac50b25b5ae97ebbd53a19c4fbc60eac36914b
sumin3 <suminyu@yahoo.com>
2018-11-20 07:45:11 +0000

    use join method

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -4,4 +4,4 @@
 str1 =  ARGV[0].scan(/\[(from:.*?)\]/).join().split(":")[1]
 str2 =  ARGV[0].scan(/\[(to:.*?)\]/).join().split(":")[1]
 str3 =  ARGV[0].scan(/\[(flags:.*?)\]/).join().split(":", 2)[1]
-puts str1 + "," +  str2 + "," + str3
+puts [str1, str2, str3].join(",")
