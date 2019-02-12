383cb020a3a6cc8a71cf6671e84cd88fb01a780c
sumin3 <suminyu@yahoo.com>
2019-02-12 01:01:08 +0000

    remove space infront of completed count

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,8 +4,9 @@
 returns information about his/her TODO list progress
 """
 if __name__ == "__main__":
-    import requests
     from sys import argv
+    import requests
+
     url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
     employee = requests.get(url).json().get('name')
 
@@ -13,7 +14,7 @@ if __name__ == "__main__":
         argv[1])
     todos = requests.get(url_todo).json()
     done = [todo for todo in todos if todo.get('completed')]
-    print("Employee {} is done with tasks ({}/{}):".format(
+    print("Employee {} is done with tasks({}/{}):".format(
         employee, len(done), len(todos)))
     for d in done:
         print("\t ", d.get('title'))
