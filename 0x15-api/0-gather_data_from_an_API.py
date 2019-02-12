92fe824fef93ef8ee984ac0f9ebbcb0d9ebbd04a
sumin3 <suminyu@yahoo.com>
2019-02-12 01:03:13 +0000

    rm empty line

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- a Python script that, using this REST API, for a given employee ID,
+Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress
 """
 if __name__ == "__main__":
